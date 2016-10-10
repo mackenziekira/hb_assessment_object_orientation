20c724a2798b9e6d57c5fc2dff2dcb9d74fe7bc4
vagrant <vagrant@vagrant.vm>
2016-10-10 01:00:17 +0000

    Cleaned up the questions a bit.

--- assessment.py
+++ assessment.py
@@ -4,17 +4,17 @@ Part 1: Discussion
 1. What are the three main design advantages that object orientation
    can provide? Explain each concept.
 
-    1) Abstraction: The inner machinery of how something is done is hidden away/compartmentalized, so if a person wants to use a class/its information/its methods, the person doesn't have to know how they are handled internally, they just have to know about the inputs and outputs, and what attributes and methods are available to them as a programmer.
-    2) Polymorphism: With subclasses and the possibility of instance attributes, you can easily make similar types of 'things' that have slightly different/unique attributes and methods without having to copy and paste the same code in a bunch of places, but rather by referencing a single class that contains in one place the information the subclasses or instances share.
-    3) Encapsulation: Information/data (attributes) is bundled with specific stuff you can do to that information (methods). 
+    1) Abstraction: The inner machinery of how something is done is hidden away/compartmentalized, so if a person wants to use a class's methods, the person doesn't have to know how they are handled internally, they just have to know about the inputs and outputs, and what attributes and methods are available to them through the class.
+    2) Polymorphism: Thanks to inheritance, you can easily make similar types of 'things,' whether it's through unique instance attributes or defining subclasses. These 'things' inherit the attributes and methods from the parent class, but can also be tweaked to have their own unique characteristics as necessary. This means you don't have to copy and paste the same code in a bunch of places, but rather can store it in one class that the subclasses or instances can reference.
+    3) Encapsulation: Information/data (attributes) is bundled with specific functions you can do to and with that information (methods). 
 
 2. What is a class?
     
-    A class is a grouping of attributes and methods that will be shared either by instances created from that class, or inherited by subclasses beneath that class.
+    A class is a grouping of attributes and methods. Instances can be created from that class, and you can make subclasses of classes.
 
 3. What is an instance attribute?
 
-    An instance attribute is an attribute that exists on that particular instance of a class. They can be set during instantiation, by including them in the init method, or they can be added once the class has been instantiated. If it shares a name with a class attribute from the class from which the instance was derived, the instance attribute would take precedence over the class attribute or any parent class attribute by the same name when Python is looking for the value belonging to the attribute.
+    An instance attribute is an attribute that exists on that particular instance of a class. Instance attributes can be set during instantiation, by including them in the init method, or they can be added once the class has been instantiated. If an instance attribute shares a name with a class attribute from the class from which the instance was derived, the instance attribute would take precedence over the class attribute or any parent class attribute by the same name when Python is looking for the value associated with the attribute.
 
 4. What is a method?
 
@@ -29,11 +29,11 @@ Part 1: Discussion
 
    A class attribute belongs to a class, whereas an instance attribute lives on each object instantiated from a class. 
 
-   That means that if you decide to instantiate a class with a particular instance attribute (or even add the instance attribute after instantiation), each instance will have a unique place in memory for its instance attribute. A thousand instances means a thousand instance attributes (assuming you initiate the object with the attribute).
+   That means that if you decide to instantiate a class with a particular instance attribute (or even add the instance attribute after instantiation), each instance will have a unique place in memory for its instance attribute. A thousand instances means a thousand instance attributes (assuming you instantiate the object with the attribute).
 
     With a class attribute, the attribute lives with the class, so all instances of the class will have access to the attribute, but the attribute will only live in one place in memory, with the class.
 
-    If you expect all or most of the instances of a class to have the same attribute, like all the members of a dog class having the species 'dog,' you'd want to use a class attribute. That way you don't have many places in memory storing the same information.
+    If you expect all or most of the instances of a class to have the same attribute, like all the members of a dog class having the species 'dog,' you'd want to store that information as a class attribute. That way you don't have many places in memory storing the same information.
 
     If you expect the instances to need unique attributes, like a person's name or a credit card number, an instance attribute would make more sense.
 
